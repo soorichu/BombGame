8184693b74829a82fd0a66be3acc3b01e271e96d
Soori <soorichu@gmail.com>
2021-05-14 14:08:24 +0900

    UI까지가 2차시로

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -7,15 +7,14 @@ public class FollowCamera : MonoBehaviour
     public Transform target;
     public float dist = 5.0f;
     public float height = 4.0f;
-    public float smoothRotate = 5.0f;
 
     void LateUpdate()
     {
-        float currYAngle = Mathf.LerpAngle(transform.eulerAngles.y, target.eulerAngles.y, 
-                                            smoothRotate * Time.deltaTime);
+//        float currYAngle = Mathf.LerpAngle(transform.eulerAngles.y, target.eulerAngles.y, 
+//                                            smoothRotate * Time.deltaTime);
 
-        Quaternion rot = Quaternion.Euler(0, currYAngle, 0);
-        transform.position = target.position - (rot * Vector3.forward * dist) + (Vector3.up * height);
+//        Quaternion rot = Quaternion.Euler(0, currYAngle, 0);
+        transform.position = target.position - (target.forward * dist) + (Vector3.up * height);
         transform.LookAt(target);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     private float translation;
     private float rotation;
+    private float jump;
+
+    private Animator ani;
+
+    private void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
 
     void Update()
     {
@@ -14,5 +22,23 @@ public class PlayerController : MonoBehaviour
 
         transform.Translate(0, 0, translation * Time.deltaTime);
         transform.Rotate(0, rotation * Time.deltaTime, 0);
+        
+        if(translation != 0 || rotation != 0)
+        {
+            ani.SetBool("walk", true);
+        }
+        else
+        {
+            ani.SetBool("walk", false);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Obstacle")
+        {
+            ani.SetTrigger("damage");
+        }
     }
 }
